8ec473821771d43b863c753c2d7e33e9e7a7e3a3
Josiya9072 <josiyachacko78@gmail.com>
2025-05-07 13:46:17 +0530

    Add files via upload

---
+++ arith.sh
@@ -0,0 +1,14 @@
+echo "enter first number:\c"
+read n1
+echo "enter second number:\c"
+read n2
+sum=$((n1+n2))
+sub=$((n1-n2))
+pro=$((n1*n2))
+div=$((n1/n2))
+rem=$((n1%n2))
+echo "sum of $n1 + $n2 =$sum" 
+echo "sub of $n1 - $n2 =$sub" 
+echo "pro of $n1 * $n2 =$pro"
+echo "div of $n1 / $n2 =$div"
+echo "rem of $n1 % $n2 =$rem"   
